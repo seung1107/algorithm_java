815365666133d95962c3a3f2fe72e065fa056282
Johnny Kim <seung6870@gmail.com>
2016-08-12 21:04:04 +0900

    버블 소트

---
+++ src/algorithm_java/BubbleSort.java
@@ -0,0 +1,21 @@
+package algorithm_java;
+
+public class BubbleSort {
+	public static void main(String[] args) {
+		int[] index = {8, 4, 7, 3, 1, 6, 5, 2};
+		int i, j, temp;
+
+		for(i=0 ; i<index.length-1 ; i++){
+			for(j=0 ; j<index.length-1-i ; j++){
+				if(index[j] > index[j+1]){
+					temp = index[j];
+					index[j] = index[j+1];
+					index[j+1] = temp;
+				}
+			}
+		}
+		for (int k = 0; k < index.length; k++) {
+			System.out.print(index[k]+" ");
+		}
+	}
+}
