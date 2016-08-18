96257a944a2d7687a3fbe3c469f512dfeb09717b
Johnny Kim <seung6870@gmail.com>
2016-08-18 21:51:39 +0900

    주석 추가

--- src/algorithm_java/QuickSort.java
+++ src/algorithm_java/QuickSort.java
@@ -4,23 +4,23 @@ public class QuickSort {
 	public void quickSort(int[] arr, int l, int r){
 		int left = l;
 		int right = r;
-		int pivot = arr[(l+r)/2];
+		int pivot = arr[(l+r)/2]; //비교 기준 설정
 		int temp;
 		
 		do{
-			while(arr[left] < pivot) left++;
-			while(arr[right] > pivot) right--;
-			if(left <= right){
+			while(arr[left] < pivot) left++; // 피벗 기준 왼쪽의 원소가 피벗 값보다 큰 값이 나올 때까지
+			while(arr[right] > pivot) right--; //피벗 기준 오른쪽의 원소가 피벗 값보다 작은 값이 나올 때까지
+			if(left <= right){ 
 				temp = arr[right];
 				arr[right] = arr[left];
 				arr[left] = temp;
 				left++;
 				right--;
-			}
-		}while(left <= right);
+			} //left, right 교체
+		}while(left <= right); //left와 right가 교차 되기 전까지만
 		
-		if(l<right) quickSort(arr, l, right);
-		if(r>left) quickSort(arr, left, r);
+		if(l<right) quickSort(arr, l, right); //피벗 기준 왼쪽 sub배열을 재귀
+		if(r>left) quickSort(arr, left, r); //피벗 기준 오른쪽 sub배열을 재귀
 	}
 	
 	public static void main(String[] args) {
